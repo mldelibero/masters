dc7a03e30e0aac8458f9349b11138a6763553cf7
Michael <mldelibero@gmail.com>
2015-05-31 16:18:47 -0400

    updated fun to read data from LTSpice file.

--- thesis/scripts/getData.m
+++ thesis/scripts/getData.m
@@ -1,8 +1,8 @@
-function [w, cdata] = getData(filename)
-    data      = csvread(filename);
-    w         = data(:,1) * 2 * pi;
-    real_data = data(:,2);
-    imag_data = data(:,3);
-    
-    cdata = real_data + 1i * imag_data;
+function [w, cData, rData, iData] = getData(filename)
+% This function parses data from an LTSpice file into a format that can be used for regression analysis
+    data  = csvread(filename);
+    w     = data(:,1) * 2 * pi;
+    rData = data(:,2);
+    iData = data(:,3);
+    cData = rData + 1i * iData;
 end
