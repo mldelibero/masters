2ce334aae7aa5d109e231d422e67c5c3f7679950
Michael <mldelibero@gmail.com>
2015-06-06 08:06:12 -0400

    transposed data to fit with new funs.

--- thesis/scripts/getData.m
+++ thesis/scripts/getData.m
@@ -1,8 +1,8 @@
 function [w, cData, rData, iData] = getData(filename)
 % This function parses data from an LTSpice file into a format that can be used for regression analysis
     data  = csvread(filename);
-    w     = data(:,1) * 2 * pi;
-    rData = data(:,2);
-    iData = data(:,3);
+    w     = (data(:,1) * 2 * pi)';
+    rData = data(:,2)';
+    iData = data(:,3)';
     cData = rData + 1i * iData;
 end
