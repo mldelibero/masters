eb53f067e47ed6ae6d7dc22764616a20e3ba4103
Michael <mldelibero@gmail.com>
2015-06-06 10:09:52 -0400

    removed plotting changed file to no not use any cheby. validated file.

--- thesis/scripts/regression_levy.m
+++ thesis/scripts/regression_levy.m
@@ -1,35 +1,16 @@
-function N = regression_levy(filename,modelOrder)
+function G = regression_levy(cData, w, modelOrder)
     % Execute the regression analysis based upon Levy's method.
-
-    % Clear environment
-    clearvars;
-    close all;
-    format shorte;
-
-    % Prepare the data
-    if ~exist('filename') || ~exist('modelOrder')
-        filename = './data/GRM31MR71H105KA88.txt';
-        modelOrder  = 5;
-    end
-
-    [w, cData, rData, iData] = getData(filename);
-    [T, w_norm] = calcChebyPoly(w);
-
-    % Solve
-    [TnumCoeff,TdenCoeff] = calcCoeffs(w_norm, T, cData, modelOrder);
-    [numCoeff] = clenshaw(TnumCoeff);
-    [denCoeff] = clenshaw(TdenCoeff);
-
-    % Plot
-    plotOrigData(cData,w);
-    plotAns(numCoeff,denCoeff,w);
+    [numCoeff, denCoeff] = calcCoeffs(w, cData, modelOrder);
+    numCoeff
+    denCoeff = [1;denCoeff]
+    [G, Num, Den] = calcDataFromCoeffs(numCoeff, denCoeff, w);
 end
 
-function [TnumCoeff, TdenCoeff] = calcCoeffs(w_norm, cheby, cData, modelOrder)
-    % This calculates the polynomial coefficients from the Chebyshev polynomial series
+function [TnumCoeff, TdenCoeff] = calcCoeffs(w, cData, modelOrder)
+    % This calculates the polynomial coefficients from the wshev polynomial series
 
     % Prepare variables
-    numSamples = size(cheby,1);
+    numSamples = size(w,1);
     matrixSize = modelOrder * 2 + 1;
 
     M = ones(matrixSize, matrixSize);
@@ -39,16 +20,22 @@ function [TnumCoeff, TdenCoeff] = calcCoeffs(w_norm, cheby, cData, modelOrder)
     sign  = 0;
     power = 0;
 
+    format = 'Row:%i, Col:%i, Q:%i, P:%i, S:%i, F:%s\n';
+
     %% Calculate
     % Populate M
     for row = 1:matrixSize
         for col = 1:matrixSize
-            
             % Find out which section of M you are in:
             if ((row <= matrixSize - modelOrder) && (col <= matrixSize - modelOrder))
+                Q = 0;
                 %Upper Left Quadrant -- lambdas
                 if (xor(mod(row,2) == 0, mod(col,2) == 0) == 1) % If both are odd or both are even
                     M(row,col) = 0;
+                    power = 0;
+                    sign = 0;
+                    F = '0';
+                    %fprintf(format,row,col,Q,power,sign,F);
                 else
                     power = row + col - 2;
                     if ((mod(col,3) == 0) && (mod(row,2) == 1))
@@ -59,10 +46,13 @@ function [TnumCoeff, TdenCoeff] = calcCoeffs(w_norm, cheby, cData, modelOrder)
                         sign = 1;
                     end
                     
-                    M(row,col) = sign * lambda(cheby,power);
-            end
+                    M(row,col) = sign * lambda(w,power);
+                    F = 'L';
+                    %fprintf(format,row,col,Q,power,sign,F);
+                end % if (xor(mod(row,2) == 0, mod(col,2) == 0) == 1) % If both are odd or both are even
             elseif (row <= matrixSize - modelOrder)
                 % Upper Right quadrant -- S and T
+                Q = 1;
                 power = col - matrixSize + modelOrder + row - 1;
                 if (col == matrixSize - modelOrder + 1)
                     if (mod(power,2) == 0)
@@ -81,13 +71,18 @@ function [TnumCoeff, TdenCoeff] = calcCoeffs(w_norm, cheby, cData, modelOrder)
                 end
                 
                 if  (mod(power,2) == 1)
-                    M(row,col) = sign * T(cheby,imag(cData),power);
+                    M(row,col) = sign * T(w,imag(cData),power);
+                    F = 'T';
                 else
-                    M(row,col) = sign * S(cheby,real(cData),power);
+                    M(row,col) = sign * S(w,real(cData),power);
+                    F = 'S';
                 end
+
+                %fprintf(format,row,col,Q,power,sign,F);
                 
             elseif (col <= modelOrder+1)
                 % Lower Left -- S and T
+                Q = 2;
                 power = col + row - matrixSize + modelOrder - 1;
                 if (mod(col,3) == 0)
                     sign = -1;
@@ -102,16 +97,25 @@ function [TnumCoeff, TdenCoeff] = calcCoeffs(w_norm, cheby, cData, modelOrder)
                 end
                 
                 if (mod(power,2) == 0)
-                    M(row,col) = sign * S(cheby,real(cData),power);
+                    M(row,col) = sign * S(w,real(cData),power);
+                    F = 'S';
                 else
-                    M(row,col) = sign * T(cheby,imag(cData),power);
+                    M(row,col) = sign * T(w,imag(cData),power);
+                    F = 'T';
                 end
+
+                %fprintf(format,row,col,Q,power,sign,F);
             else
                 % Lower Right -- U
+                Q = 3;
                 locrow = row - matrixSize + modelOrder;
                 loccol = col - matrixSize + modelOrder;
                 if (xor(mod(locrow,2) == 0, mod(loccol,2) == 0) == 1) % If both are odd or both are even
                     M(row,col) = 0;
+                    power = 0;
+                    sign = 0;
+                    F = '0';
+                    %fprintf(format,row,col,Q,power,sign,F);
                 else
                     power = locrow + loccol;
                     if     ((mod(locrow,2) == 1) && (mod(loccol,3) == 0))
@@ -122,7 +126,9 @@ function [TnumCoeff, TdenCoeff] = calcCoeffs(w_norm, cheby, cData, modelOrder)
                         sign = 1;
                     end
                     
-                    M(row,col) = sign * U(cheby,real(cData),imag(cData),power);
+                    M(row,col) = sign * U(w,real(cData),imag(cData),power);
+                    F = 'U';
+                    %fprintf(format,row,col,Q,power,sign,F);
                 end
             end
         end
@@ -132,21 +138,22 @@ function [TnumCoeff, TdenCoeff] = calcCoeffs(w_norm, cheby, cData, modelOrder)
         if (row <= matrixSize - modelOrder)
             power = row - 1;
             if (mod(power,2) == 0)
-                C(row) = S(cheby,real(cData),power);
+                C(row) = S(w,real(cData),power);
             else
-                C(row) = T(cheby,imag(cData),power);
+                C(row) = T(w,imag(cData),power);
             end
         else
             localRow = row - matrixSize + modelOrder;
             if (mod(localRow,2) == 1)
                 C(row) = 0;
             else
-            C(row) = U(cheby,real(cData),imag(cData),localRow);
+            C(row) = U(w,real(cData),imag(cData),localRow);
             end
         end
     end
     
     % Solve For N
+    %M(1:size(M,2),1)
     N = inv(M) * C;
     TnumCoeff = N(1:(matrixSize+1)/2);
     TdenCoeff = N(  (matrixSize+1)/2+1:matrixSize);
@@ -180,67 +187,4 @@ function [TnumCoeff, TdenCoeff] = calcCoeffs(w_norm, cheby, cData, modelOrder)
         end
     end
 end % function [b,a] = calcCoeffs(w_norm, T, cData)
-
-function [coeff] = clenshaw(chebyCoeff)
-    % This function transforms chebyshev coefficients into power series coefficients
-    coeff = chebyCoeff;
-
-    n  = length(coeff);
-    y2 = zeros(n);
-    y1 = zeros(n);
-    y0 = zeros(n);
-    
-    %yk = 2*w*y1 - y2 + chebyCoeff[k)
-
-    for k=1:n
-        for index = n:-1:2
-            %Left shift to multiply by w
-            y0(index) = y1(index - 1);
-        end
-        yo = y0 .* 2 - y2;
-        y0(1) = y0(1) + chebyCoeff(k);
-    end
-end % function [coeff] = clenshaw(chebyCoeff)
-
-function plotOrigData(cData,w)
-    % Plot
-    figure;
-    rows = 1;
-
-    cols = 2;
-
-    subplot(rows,cols,1);
-    semilogx(w,abs(cData));
-    title('Magnitude');
-
-    subplot(rows,cols,2);
-    semilogx(w,rad2deg(phase(cData)));
-    title('Phase');
-    end
-
-function plotAns(numCoeff,denCoeff,w)
-    % Calculate output from coefficients 
-    Num = numCoeff(1) * ones(size(w,1),1);
-    Den = ones(size(w,1),1);
-
-    for h = 1:size(denCoeff);
-        Num = Num + numCoeff(h+1) .* (1i * w).^h;
-        Den = Den + denCoeff(h)   .* (1i * w).^h;
-    end
-
-    G = Num ./ Den;
-
-    % Plot
-    figure;
-    rows = 1;
-    cols = 2;
-
-    subplot(rows,cols,1);
-    semilogx(w,abs(G));
-    title('Magnitude');
-
-    subplot(rows,cols,2);
-    semilogx(w,rad2deg(phase(G)));
-    title('Phase');
-end
 
