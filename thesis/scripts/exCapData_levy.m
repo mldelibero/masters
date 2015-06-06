47b3d47c0bb0cbe7cc3ea219baa59f057d47ba7c
Michael <mldelibero@gmail.com>
2015-06-06 15:03:38 -0400

    moved to run_levy_iter_RealCap.m

--- thesis/scripts/exCapData_levy.m
+++
@@ -1,9 +0,0 @@
-function exCapData_levy
-    clearvars;
-    close all;
-    format shorte;
-
-    filename = 'GRM31MR71H105KA88.txt';
-    modelOrder  = 5;
-    regression_levy(filename,modelOrder);
-end
