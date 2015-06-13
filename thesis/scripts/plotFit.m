2b75d5f672ca10b48a4bcd2db0a7c5bfdd2431b4
Michael <mldelibero@gmail.com>
2015-06-13 10:58:24 -0400

    changed formatting so that more cases can use it.

--- thesis/scripts/plotFit.m
+++ thesis/scripts/plotFit.m
@@ -10,7 +10,7 @@ function plotFit(pType, Data1, Data2, x)
 
     %% Plot specific request
     if pType == plotType.cData
-        [ax,p1,p2] = plotyy(x,mag2db(abs(Data1)),x,rad2deg(phase(Data1)),'semilogx','semilogx');
+        [ax,p(1),p(2)] = plotyy(x,mag2db(abs(Data1)),x,rad2deg(phase(Data1)),'semilogx','semilogx');
 
         title('GRM31MR71H105KA88','FontSize',titleSize);
         h_leg = legend('Magnitude','Phase','Location','north');
@@ -30,32 +30,38 @@ function plotFit(pType, Data1, Data2, x)
         rows = 2;
         cols = 2;
 
-        subplot(rows,cols,1);
-        loglog(x,abs(Data1)); hold on;
-        loglog(x,abs(Data2));
+        ax(1) = subplot(rows,cols,1);
+        p(1)  = semilogx(x,mag2db(abs(Data1))); hold on;
+        p(2)  = semilogx(x,mag2db(abs(Data2)));
         title('Magnitude','FontSize',titleSize);
         h_leg = legend('Orig','FitData','Location','north');
         set(h_leg,'FontSize',legendSize);
         xlabel('\omega'  ,'FontSize',axisTitleSize);
         ylabel('Mag (dB)','FontSize',axisTitleSize);
+        min_y1 = min(get(ax(1),'ytick'));
+        max_y1 = max(get(ax(1),'ytick'));
+        set(ax(1),'ytick',min_y1:20:max_y1);
 
-        subplot(rows,cols,2);
-        semilogx(x,rad2deg(phase(Data1))); hold on;
-        semilogx(x,rad2deg(phase(Data2)));
+        ax(2) = subplot(rows,cols,2);
+        p(3)  = semilogx(x,rad2deg(phase(Data1))); hold on;
+        p(4)  = semilogx(x,rad2deg(phase(Data2)));
         title('Phase','FontSize',titleSize);
         h_leg = legend('Orig','FitData','Location','north');
         set(h_leg,'FontSize',legendSize);
         xlabel('\omega','FontSize',axisTitleSize);
         ylabel('\phi (deg)','FontSize',axisTitleSize);
+        min_y2 = min(get(ax(2),'ytick'));
+        max_y2 = max(get(ax(2),'ytick'));
+        set(ax(2),'ytick',min_y2:20:max_y2);
 
-        subplot(rows,cols,3);
-        semilogx(x,abs(Data2)-abs(Data1));
+        ax(3) = subplot(rows,cols,3);
+        p(5)  = semilogx(x,abs(Data2)-abs(Data1));
         title('Magnitude Error','FontSize',titleSize);
         xlabel('\omega','FontSize',axisTitleSize);
         ylabel('\Delta Mag (\Omega)','FontSize',axisTitleSize);
 
-        subplot(rows,cols,4);
-        semilogx(x,rad2deg(phase(Data2))-rad2deg(phase(Data1)));
+        ax(4) = subplot(rows,cols,4);
+        p(6)  = semilogx(x,rad2deg(phase(Data2))-rad2deg(phase(Data1)));
         title('Phase Error','FontSize',titleSize);
         xlabel('\omega','FontSize',axisTitleSize);
         ylabel('\Delta \phi (deg)','FontSize',axisTitleSize);
@@ -63,8 +69,8 @@ function plotFit(pType, Data1, Data2, x)
     elseif pType == plotType.oneError
         rows = 1;
         cols = 1;
-        subplot(rows,cols,1);
-        plot(Data1);
+        ax = subplot(rows,cols,1);
+        p  = plot(Data1);
         title('Norm Magnitude Err^2 + Norm Phase Err^2','FontSize',titleSize);
         xlabel('n','FontSize',axisTitleSize);
         ylabel('Error','FontSize',axisTitleSize);
@@ -73,27 +79,27 @@ function plotFit(pType, Data1, Data2, x)
         rows = 1;
         cols = 2;
 
-        subplot(rows,cols,1);
-        semilogy(Data1);
+        ax(1) = subplot(rows,cols,1);
+        p (1) = semilogy(Data1);
         title('Magnitude Err^2','FontSize',titleSize);
         xlabel('n','FontSize',axisTitleSize);
         ylabel('Error^2 ((\Delta \Omega)^2)','FontSize',axisTitleSize);
 
-        subplot(rows,cols,2);
-        semilogy(Data2);
+        ax(2) = subplot(rows,cols,2);
+        p (2) = semilogy(Data2);
         title('Phase Error^2','FontSize',titleSize);
         xlabel('n','FontSize',axisTitleSize);
         ylabel('Error^2 ((\Delta \Phi)^2)','FontSize',axisTitleSize);
     end % if pType == plotType.cVectorsDiff
 
     %% Common plotting options
-    grid(ax(1),'on');
-    p1.LineWidth = 2;
-    set(ax(1),'FontSize',axisSize);
-    if (size(ax,2) == 2)
-        p2.LineWidth = 2;
-        set(ax(2),'FontSize',axisSize);
-    end
+    for ind = 1:length(ax)
+        grid(ax(ind),'on');
+        set(ax(ind),'FontSize',axisSize);
+    end % for ind = 1:length(ax)
 
+    for ind = 1:length(p)
+        p(ind).LineWidth = 2;
+    end % for ind = 1:length(p)
 end % function plotFit()
 
