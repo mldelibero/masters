a2a2380a8dab367d7227a0c5892b9339ab1de5af
Michael <mldelibero@gmail.com>
2015-06-13 17:18:40 -0400

    added basice lse output.

---
+++ thesis/scripts/run_basic_lse.m
@@ -0,0 +1,28 @@
+% This script runs the basic LSE for a line
+
+% Clear environment
+clearvars;
+close all;
+format shorte;
+
+%% Generate data
+n = 5;
+err = rand(1,n)*1.3;
+x = linspace(1,n,n);
+b = rand(1)*ones(1,n);
+y = x + err + b;
+
+%% LSE
+avg_y   = sum(y);
+avg_x   = sum(x);
+avg_xy  = sum(y .* x);
+avg_xsq = sum(x .^ 2);
+
+a_1 = (avg_xy - avg_x *avg_y) / (avg_xsq - avg_x^2)
+a_0 = avg_y - a_1 * avg_x
+
+y_lse = a_0*ones(1,length(x)) + a_1 .* x;
+
+%% Plot
+plotFit(plotType.DIFF_PLOT,y,y_lse,x);
+
