ef0d5e400b84f27b322dcdacb9e04e141399b92f
Michael <mldelibero@gmail.com>
2015-06-13 13:31:41 -0400

    added script to run the full model and generate plots.

---
+++ thesis/scripts/run_fullModel.m
@@ -0,0 +1,49 @@
+% This script runs the iterative regression analysis to fit a RSL capacitor model
+
+% Clear environment
+clearvars;
+close all;
+format shorte;
+
+modelType = modelTypes.FULL_MODEL;
+NumDeg = 4;
+DenDeg = 3;
+iterations = 100;
+filename = './data/GRM31MR71H105KA88.txt';
+
+
+[w, cData, rData, iData] = getData(filename);
+[initDen] = getInitGuess(w,modelType);
+[G, numCoeffs, denCoeffs, E, minIndex] = regression_levy_iter(cData, w, iterations, NumDeg, DenDeg, initDen);
+
+%% Error minimization
+Emag = E(:,1);
+Epha = E(:,2);
+EmagNorm = Emag ./ max(Emag);
+EphaNorm = Epha ./ max(Epha);
+E2 = EmagNorm + EphaNorm;
+n = find(E2==min(E2),1);
+fprintf('Error Minimized at Iteration: %i\n',n);
+
+%% Calculate Circuit Parameters
+%[LE, RE, RL, C, CD, RD] = calcCircParams(numCoeffs, denCoeffs);
+params = calcCircParams(numCoeffs, denCoeffs, modelType);
+
+
+%% Plot 
+plotcDiff = plotType.cVectorsDiff;
+plotErrs  = plotType.twoErrors;
+plotErr   = plotType.oneError;
+
+plotFit(plotcDiff, cData, G(n,:), w); % figures/modeling/levyIter.jpg
+plotFit(plotErrs , Emag,  Epha     ); % figures/modeling/levyIter_Err1.jpg
+plotFit(plotErr  , E2              ); % figures/modeling/levyIter_Err2.jpg
+
+%% Plot Circuit Parameters
+rows = 1;
+cols = 2;
+figure;
+subplot(rows,cols,1); plot(params(1:10,1)); title('LE');
+subplot(rows,cols,2); plot(params(1:10,2)); title('RE');
+
+
