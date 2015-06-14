b957b1461f53642c399170e5505ac7d7a1ab09bc
Michael <mldelibero@gmail.com>
2015-06-14 15:20:14 -0400

    now generating capMag as cap in a matlab script.

---
+++ thesis/scripts/params/c.m
@@ -0,0 +1,13 @@
+% Run this to generate a plot for an RC low pass filter with a varying capacitance.
+clearvars;
+close all;
+
+w = 2*pi*logspace(-1,6,100);
+C = 1*10^-6;
+
+Zc = 1 ./ (1i .* w .* C);
+
+[h_fig, h_leg, h_title, ax, p] = plotFit(plotType.cData, Zc, Zc, w);
+h_leg.Location = 'northeast';
+h_title.String = 'Capacitor Impedance';
+
