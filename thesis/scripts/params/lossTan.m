cdb6f1a68589f64396dd49ceeb6a00abdfeef00d
Michael <mldelibero@gmail.com>
2015-07-12 17:52:33 -0400

    started working on the loss tangent upgrade.

--- thesis/scripts/params/lossTan.m
+++ thesis/scripts/params/lossTan.m
@@ -1,6 +1,14 @@
+% scripts/params/lossTan.m
 % Run this to generate a plot for the loss tangent of a capacitor
+
+% Clear environment
 clearvars;
 close all;
+format shorte;
+
+% Add funs to path
+addpath('../utilityFuns');
+addpath('../');
 
 theta = pi / 9; % 20 degrees
 esr = [0,1];
@@ -11,4 +19,16 @@ xlabel(ax,'ESR')
 ylabel(ax,'jX_C');
 h_title.String = 'Loss Tangent';
 set(gca,'Xtick',[],'Ytick',[]);
+
+%%
+%figure;
+xstart = 0.3 * max(esr);
+r = 1.8*xstart;
+x = linspace(xstart,r,10);
+%xf = fliplr(x);
+%xt = [x,xf];
+
+y = [sqrt(r^2-x.^2)];%,-sqrt(r^2-xf.^2)];
+hold on;
+plot(x,y);
 
